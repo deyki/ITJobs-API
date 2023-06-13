689f410ce48510d8cd8f510aaada01b030eb85b0
deyki <deikioveca@gmail.com>
2023-06-13 18:44:42 +0300

    validateTokenAndRetrieveSubject method added

--- user-service/src/main/java/com/deyki/userservice/security/JWTUtil.java
+++ user-service/src/main/java/com/deyki/userservice/security/JWTUtil.java
@@ -1,8 +1,11 @@
 package com.deyki.userservice.security;
 
 
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import com.auth0.jwt.JWT;
 import org.springframework.stereotype.Component;
@@ -22,5 +25,13 @@ public class JWTUtil {
                 .withIssuedAt(new Date())
                 .withIssuer("ITJobs-API-user-service")
                 .sign(Algorithm.HMAC256(secret));
+    }
+    public String validateTokenAndRetrieveSubject(String token) throws JWTVerificationException {
+        JWTVerifier jwtVerifier = JWT.require(Algorithm.HMAC256(secret))
+                .withSubject("User Details")
+                .withIssuer("ITJobs-API-user-service")
+                .build();
+        DecodedJWT decodedJWT = jwtVerifier.verify(token);
+        return decodedJWT.getClaim("username").asString();
     }
 }
