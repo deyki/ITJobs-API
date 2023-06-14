9727927f40dfb1426b8ac153690cc6a73f83619d
deyki <deikioveca@gmail.com>
2023-06-14 16:30:03 +0300

    ExceptionHandler

---
+++ job-service/src/main/java/com/deyki/jobservice/error/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.deyki.jobservice.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.reactive.result.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@ResponseStatus
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ProblemDetail userNotFoundException(UserNotFoundException exception) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, exception.getMessage());
+    }
+}
