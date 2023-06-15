9a95cb628629a7a186c598a4593a6257759164f4
deyki <deikioveca@gmail.com>
2023-06-15 14:23:53 +0300

    ""

--- job-application-service/src/main/java/com/deyki/jobapplicationservice/JobApplicationServiceApplication.java
+++ job-application-service/src/main/java/com/deyki/jobapplicationservice/JobApplicationServiceApplication.java
@@ -2,13 +2,12 @@ package com.deyki.jobapplicationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 public class JobApplicationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JobApplicationServiceApplication.class, args);
 	}
+
 }
