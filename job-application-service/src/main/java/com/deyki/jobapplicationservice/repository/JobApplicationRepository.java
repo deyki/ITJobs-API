dede8d8cf751176d2728d223b765b8663a6e9611
deyki <deikioveca@gmail.com>
2023-06-15 21:01:23 +0300

    New query added

--- job-application-service/src/main/java/com/deyki/jobapplicationservice/repository/JobApplicationRepository.java
+++ job-application-service/src/main/java/com/deyki/jobapplicationservice/repository/JobApplicationRepository.java
@@ -2,8 +2,14 @@ package com.deyki.jobapplicationservice.repository;
 
 import com.deyki.jobapplicationservice.entity.JobApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface JobApplicationRepository extends JpaRepository<JobApplication, Long> {
+    @Query("SELECT j FROM JobApplication j WHERE j.jobID = ?1")
+    Optional<JobApplication> findByJobId(@Param("jobID") Long jobID);
 }
