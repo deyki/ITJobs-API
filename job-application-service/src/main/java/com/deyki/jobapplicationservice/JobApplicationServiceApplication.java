c5df7319128bc79c92bcb942bfe74bc05275301a
Nikolay Nikolaev <111266016+deyki@users.noreply.github.com>
2023-06-15 14:06:17 +0300

    Add files via upload

---
+++ job-application-service/src/main/java/com/deyki/jobapplicationservice/JobApplicationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.deyki.jobapplicationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobApplicationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobApplicationServiceApplication.class, args);
+	}
+
+}
