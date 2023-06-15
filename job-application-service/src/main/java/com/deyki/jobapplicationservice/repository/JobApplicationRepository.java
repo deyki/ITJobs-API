1be2e9d0c1bb354d79ceda8b183b33cd86c79d57
deyki <deikioveca@gmail.com>
2023-06-15 16:04:02 +0300

    JobApplicationRepository

---
+++ job-application-service/src/main/java/com/deyki/jobapplicationservice/repository/JobApplicationRepository.java
@@ -0,0 +1,9 @@
+package com.deyki.jobapplicationservice.repository;
+
+import com.deyki.jobapplicationservice.entity.JobApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JobApplicationRepository extends JpaRepository<JobApplication, Long> {
+}
